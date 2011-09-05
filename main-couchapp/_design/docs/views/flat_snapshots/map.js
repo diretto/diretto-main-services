920a27e7c5298796c5e23272f266a5a688152029
berb <mail@benjamin-erb.de>
2011-09-05 12:49:53 +0000

    main couchapp: new view
    git-svn-id: https://svn.illmatix.de/svn/ubi/v2/services@1868 d2255250-64ce-4612-be3a-de8aecb87a1d

---
+++ main-couchapp/_design/docs/views/flat_snapshots/map.js
@@ -0,0 +1,16 @@
+/**
+ * @author Benjamin Erb
+ */
+function(doc) {
+
+	  if (doc.type === "snapshot") {
+		  emit(doc.documentId,["document",doc.documentId,"document",doc.documentId]);
+		  emit(doc.documentId,["document",doc.documentId,"attachment",doc.documentId]);
+		  emit(doc.documentId,["document",doc.documentId,"time",(""+doc.time.after+"--"+doc.time.before)]);
+		  emit(doc.documentId,["document",doc.documentId,"location",(""+doc.location.lat+","+doc.location.lon+","+doc.location.variance)]);
+		  
+		  doc.tags.forEach(function(tagId){
+			  emit(doc.documentId,["document",doc.documentId,"tag",tagId]);
+		  });
+	  }
+};
