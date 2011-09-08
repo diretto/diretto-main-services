24bb220443a82c19fa2931ab2ffef07364f65397
berb <mail@benjamin-erb.de>
2011-09-08 10:21:48 +0000

    feed node: * added document positions feed
    git-svn-id: https://svn.illmatix.de/svn/ubi/v2/services@1913 d2255250-64ce-4612-be3a-de8aecb87a1d

--- main-nodes/lib/feed-node/api/geofeed.js
+++ main-nodes/lib/feed-node/api/geofeed.js
@@ -76,8 +76,69 @@ module.exports = function(h) {
 		},
 
 		getDocumentPositions : function(req, res, next) {
-			res.send(501);
-			return next();
+			h.util.dbFetcher.fetchDocumentResources(["document",req.uriParams.documentId, "location"],function(err, result){
+				if(err){
+					h.responses.error(500,"Internal server error.",res,next);
+				}
+				else if(h.util.empty(result)){
+					//empty result, so check if document exists at all
+					h.util.dbFetcher.exist(req.uriParams.documentId, h.c.DOCUMENT, function(code){
+						if(code === 200){
+							res.send(200, h.util.renderer.locationList(req.uriParams.documentId,{}));
+							return next();
+						}
+						else if(code === 404){
+							h.responses.error(404,"Document not found.",res,next);
+						}
+						else{
+							h.responses.error(500,"Internal server error.",res,next);
+						}
+					});
+				}
+				else{
+					
+					var entries = [];
+					
+					for(var idx in result[req.uriParams.documentId]["location"]){
+						if(result[req.uriParams.documentId]["location"].hasOwnProperty(idx)){
+							var item = result[req.uriParams.documentId]["location"][idx];
+							
+							entries.push({
+								userUri : h.util.coreUri.user(item.doc.publisher),
+								documentId : item.doc.documentId,
+								documentUri : h.util.coreUri.document(item.doc.documentId),
+								locationUri : h.util.coreUri.documentLocation(item.doc.documentId,item.doc.lat,item.doc.lon,item.doc.variance),
+								lat : item.doc.lat,
+								lon : item.doc.lon,
+								variance : item.doc.variance,
+								upvotes : item.votes.up,
+								downvotes : item.votes.down
+							});
+						}
+					}
+					
+
+				
+					
+					var headers = {
+							"Content-Type" : "application/vnd.google-earth.kml+xml"
+					};
+					
+					var data = {
+						serverName : h.options.server.name,
+						serverVersion : h.options.server.version,
+						title : h.options.feed.deployment.title,
+						
+						entries : entries,
+						documentId : req.uriParams.documentId
+					};
+					
+					var feed = h.mustache.to_html(template.documentPositions, data)
+					
+					res.sendArbitrary(200, feed,headers);
+					return next();
+				}
+			});
 		}
 
 	};
