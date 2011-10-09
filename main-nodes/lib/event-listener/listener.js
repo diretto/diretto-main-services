a15b9ae2be54d154b5ac0e4840f7d5a049679740
berb <mail@benjamin-erb.de>
2011-10-09 21:47:20 +0000

    main nodes: * event listener
    git-svn-id: https://svn.illmatix.de/svn/ubi/v2/services@2063 d2255250-64ce-4612-be3a-de8aecb87a1d

---
+++ main-nodes/lib/event-listener/listener.js
@@ -0,0 +1,26 @@
+var follow = require('follow');
+var events = require('events');
+
+module.exports = function(dbUri) {
+	
+	var emitter = new events.EventEmitter();
+	emitter.setMaxListeners(20);
+	
+	follow({
+			"db"  :dbUri ,
+			"since" : "now",
+			"include_docs" : true,
+			"filter" : "events/events",
+		}, function(error, change) {
+		  if(!error) {
+		    emitter.emit(change.doc.event.type, change.doc);
+		  }
+	});
+	
+	return {
+		get : function(){
+			return emitter;
+		}
+	}
+};
+
