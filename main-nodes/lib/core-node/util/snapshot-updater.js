b9a0abb883a75f6bba5a6f74c714d1a9a9af3cf2
berb <mail@benjamin-erb.de>
2011-10-12 20:13:53 +0000

    main node: snapshot updater stub
    git-svn-id: https://svn.illmatix.de/svn/ubi/v2/services@2079 d2255250-64ce-4612-be3a-de8aecb87a1d

---
+++ main-nodes/lib/core-node/util/snapshot-updater.js
@@ -0,0 +1,15 @@
+var request = require('request')
+
+/**
+ * An asynchronous updater for snapshot documents based on changes on:
+ *  tags appended, new location, new time, vote for location and vote for time 
+ */
+module.exports = function(h, eventEmitter) {
+
+
+	
+//	eventEmitter.on("DOCUMENT_CREATED", function(event) {
+//	});
+
+
+};
